7205fba9e37a9f02458302c424c5150c44f54700
sourour <ssourour131@gmail.com>
2021-12-22 19:01:04 +0100

    first commit

---
+++ app/src/main/java/com/example/orderfoodapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.orderfoodapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.orderfoodapp.model.RestaurantModel;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Restaurant List");
+
+        List<RestaurantModel> restaurantModelList = getRestaurantData();
+        initRecyclerView();
+    }
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter();
+    }
+    private List<RestaurantModel> getRestaurantData(){
+        InputStream is = getResources().openRawResource(R.raw.restaurent);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        }catch (Exception e){
+
+        }
+        String jsonStr = writer.toString();
+        Gson gson = new Gson();
+        RestaurantModel[] restaurantModels = gson.fromJson(jsonStr, RestaurantModel[].class);
+        List<RestaurantModel> restList = Arrays.asList(restaurantModels);
+
+        return restList;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/orderfoodapp/model/RestaurantModel.java
@@ -0,0 +1,4 @@
+package com.example.orderfoodapp.model;
+
+public class RestaurantModel {
+}
